bf291cb1182c658b9ee03f7192308cf1ab6165a2
MigueLucky <miguejorda8@gmail.com>
2024-09-17 10:27:59 +0200

    estructura de archivos

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>LuckyEvents</title>
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    <div>
+
+    </div>
+    <main>
+        
+    </main>
+    <div>
+        
+    </div>
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
